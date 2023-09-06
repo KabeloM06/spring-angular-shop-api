ca69e61d0f41431cda0a7a583551ffe0cecf1f19
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2023-09-06 11:48:50 +0200

    Add ability to search by category ID

--- src/main/java/com/kabelo/springangularshop/dao/ProductCategoryRepository.java
+++ src/main/java/com/kabelo/springangularshop/dao/ProductCategoryRepository.java
@@ -3,7 +3,9 @@ package com.kabelo.springangularshop.dao;
 import com.kabelo.springangularshop.Entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")//name of JSON entry and path will be '/product-category'
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
